9545e11c2fd3da337bb032cb31702a4c0a5dee77
Rontim <rongitonga@gmail.com>
2023-10-05 17:56:43 +0300

    Added a nav to events route layout

--- src/app/event/layout.js
+++ src/app/event/layout.js
@@ -5,7 +5,10 @@ export default function DashboardLayout({ children }) {
   return (
     <section className="grid grid-flow-col grid-cols-[1.5fr_2fr_1.5fr] w-full mt-0 gap-1 justify-stretch">
       <LeftSideBar />
-      <main className="dark:bg-gray-900">{children}</main>
+      <main className="dark:bg-gray-900">
+        <nav>nav</nav>
+        {children}
+      </main>
       <RightSideBar />
     </section>
   );
