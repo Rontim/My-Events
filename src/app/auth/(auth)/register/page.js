eea904bb77448316d61b98a137ac030f8a1a3eeb
Rontim <rongitonga@gmail.com>
2023-11-03 06:27:12 +0300

    Added a username field in registration form

--- src/app/auth/(auth)/register/page.js
+++ src/app/auth/(auth)/register/page.js
@@ -104,6 +104,22 @@ const Register = () => {
                 required
               />
             </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your username
+              </label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="john123"
+                required
+              />
+            </div>
             <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
               <div className="max-[640px]:col-span-2">
                 <label
